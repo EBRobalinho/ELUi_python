d43487dfe8ce64deea5d4ef39e9d83c1c7977c08
EB Robalinho <edu.robalinho@gmail.com~>
2023-11-29 02:37:05 -0300

    Comentários e início da plotagem da curva de ELU e ELUi

--- verf.py
+++ verf.py
@@ -19,12 +19,13 @@ steel = Aco(50)  # CA50, colocar , 50 mas o aço aguenta 500 Mpa
 
 # Posição das camadas de aço
 dt = 0.025  # distância da armadura à borda da seção
-nc = 2  # número de camadas de aço
 yc = [h / 2 - dt, -h / 2 + dt]  # posição das camadas de aço
 n_barras = np.array([3, 3])  # número de barras por camada
-Area = 0.25 * (16 ** 2) * (10 ** -6) * np.pi * n_barras  # área de aço por camada
 
-Area_num = 0.4 * b * h * concreto.sigma_cd / steel.fyd
+omega=0.4 #Adimensional da área de aço total na estrutura
+
+Area_num = omega * b * h * concreto.sigma_cd / steel.fyd #Área de aço an estrutura em m²
+
 Area = [(Area_num) / 2, (Area_num) / 2]
 
 # Diagrama no espaço de deformações da seção
@@ -34,17 +35,30 @@ dig = Diagrama(concreto, yc, yt, yb, h)
 nr = NewtonRaphson(10 ** -10, 1000, 10 ** -10, [0, 0],2)
 
 # Adimensionais
-nu = 0.3
-mi = 0.1
+nu = 0.3 #Adimensional do esforço solicitante normal
+mi = 0.1 #Adimensional do momento fletor solicitante
 
 # Verificação por diferenças finitas
 M_0 = concreto.sigma_cd * b * h * h * mi
-m = 100
-L = 5
 N_0 = concreto.sigma_cd * b * h * nu
+
+m = 10 #Número de elementos na diferença finita
+L = 5 #Comprimento do pilar
+
 f_inic = 0
 tol_f = 10 ** -10
 sol = Dif_fin_ver(nr, concreto, steel, yc, Area, b, h, yt, yb, m, f_inic, L, N_0, M_0, tol_f)
+
+
+d=dig.diagrama_def(12000)
+N=np.zeros(len(d))
+M=np.zeros(len(d))
+for i in range(0,len(d)):
+    [N[i],M[i]]=vetor_esfor(concreto, b, h, yt, yb, steel, yc, Area, d[i,0], d[i,1])
+
+plt.plot(M[:]/(concreto.sigma_cd * b * h * h ), N[:]/(concreto.sigma_cd * b * h) , '--')
+plt.plot(sol[:, 1]/(concreto.sigma_cd * b * h*h),sol[:, 0]/(concreto.sigma_cd * b * h), '*')
+plt.show()
 
 # Plotar resultados
 plt.plot(100 * sol[:, 2] / h, sol[:, 4] * L / m, '*')
